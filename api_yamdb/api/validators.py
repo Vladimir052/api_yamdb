dfde6491ac0df76293f61fa2f57393f865ac5ba8
WonderVika <ladyvixtwix@mail.ru>
2022-11-16 19:22:33 +0300

    added routing, added views, fixed settings, added mixins

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,56 @@
+import uuid
+
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .mixins import ListCreateDeleteViewSet, UpdateDeleteViewSet
 
 from reviews.models import User
 from .permissions import (AdminOnly, AdminOrReadOnly,
                           OwnerAdminModeratorOrReadOnly)
 from .serializers import (ConfirmCodeSerializer, EmailSerializer,
                           UserSerializer, UserInfoSerializer)
+from .utils import send_confirm_code
+
+
+@api_view(['POST'])
+def confirmation_code(request):
+    """Send confirmation_code by email"""
+    serializer = EmailSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data['username']
+    email = serializer.validated_data['email']
+    if not User.objects.filter(username=username).exists():
+        user = User.objects.create_user(username=username, email=email)
+        send_confirm_code(user.email)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    user = get_object_or_404(User, username=username, email=email)
+    send_confirm_code(user.email)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def get_jwt_token(request):
+    """Check confirmation_code and send JWT-token"""
+    serializer = ConfirmCodeSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    username = serializer.validated_data['username']
+    user = get_object_or_404(User, username=username)
+    confirmation_code = serializer.validated_data['confirmation_code']
+    uuid_code = str(uuid.uuid3(uuid.NAMESPACE_DNS, user.email))
+    if uuid_code == confirmation_code:
+        token = AccessToken.for_user(user)
+        return Response({'token': str(token)}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
