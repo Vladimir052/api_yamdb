d111282a96311085f7667eb16cea91fba48d628d
WonderVika <ladyvixtwix@mail.ru>
2022-11-16 19:56:19 +0300

    fixed admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import User
 
-admin.site.register(User)
+from .models import Category, Comment, Genre, Review, Title, User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role')
+    list_filter = ("role", )
+    search_fields = ("username__startswith", )
