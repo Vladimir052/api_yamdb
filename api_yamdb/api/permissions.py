c84bf43341c4f66a6fee4238e8302d6a5adcc7c3
WonderVika <ladyvixtwix@mail.ru>
2022-11-17 17:38:48 +0300

    fixed code in accordance with pep8, sort imports using isort

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class OwnerAdminModeratorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,20 +4,16 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from .mixins import ListCreateDeleteViewSet, UpdateDeleteViewSet
-
 from reviews.models import User
-from .permissions import (AdminOnly, AdminOrReadOnly,
-                          OwnerAdminModeratorOrReadOnly)
+
+from .permissions import AdminOnly
 from .serializers import (ConfirmCodeSerializer, EmailSerializer,
-                          UserSerializer, UserInfoSerializer)
+                          UserInfoSerializer, UserSerializer)
 from .utils import send_confirm_code
+# from .mixins import ListCreateDeleteViewSet, UpdateDeleteViewSet
 
 
 @api_view(['POST'])
