910b613a7979122dc6e9daad4ffc7b8b6fea27c5
Vladimir ishutov <ishutov52@yandex.ru>
2022-11-17 17:48:34 +0300

    add model Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -42,3 +42,34 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='отзыв'
+    )
+    text = models.CharField(
+        'текст комментария',
+        max_length=200
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='автор'
+    )
+    pub_date = models.DateTimeField(
+        'дата публикации',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return self.text
